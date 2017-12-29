5732fa1ec1878180ec54b92768bc4d3de78fa556
Ngô Hải Yến <yourgod1989@gmail.com>
2017-12-29 09:53:21 +0700

    update db

--- calllogit.sql
+++ calllogit.sql
@@ -16,6 +16,39 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comment` (
+  `comment_id` int(10) NOT NULL AUTO_INCREMENT,
+  `request_id` int(10) NOT NULL,
+  `employee_id` int(10) NOT NULL,
+  `content` text,
+  `type` tinyint(1) DEFAULT NULL,
+  `note` varchar(255) DEFAULT NULL,
+  `create_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`comment_id`),
+  KEY `fk_tickets_ticket_thread` (`request_id`),
+  KEY `fk_ticket_thread_employee` (`employee_id`),
+  CONSTRAINT `fk_ticket_thread_employee` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`employee_id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_tickets_ticket_thread` FOREIGN KEY (`request_id`) REFERENCES `request` (`request_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comment`
+--
+
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `employees`
 --
 
@@ -23,18 +56,18 @@ DROP TABLE IF EXISTS `employees`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `employees` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `employee_id` int(10) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
   `phone` int(20) NOT NULL,
-  `address` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
   `username` varchar(15) DEFAULT NULL,
-  `password` varchar(15) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
   `role` int(11) NOT NULL,
-  `itteam` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `itteam` (`itteam`),
+  `subteam_id` int(11) NOT NULL,
+  PRIMARY KEY (`employee_id`),
+  KEY `itteam` (`subteam_id`),
   KEY `role` (`role`),
-  CONSTRAINT `employees_ibfk_1` FOREIGN KEY (`itteam`) REFERENCES `itteam` (`id`),
+  CONSTRAINT `employees_ibfk_1` FOREIGN KEY (`subteam_id`) REFERENCES `itteam` (`itteam_id`),
   CONSTRAINT `employees_ibfk_2` FOREIGN KEY (`role`) REFERENCES `role` (`role_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=23445 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -45,8 +78,82 @@ CREATE TABLE `employees` (
 
 LOCK TABLES `employees` WRITE;
 /*!40000 ALTER TABLE `employees` DISABLE KEYS */;
-INSERT INTO `employees` VALUES (1,'Xuan',1234,'HaNoi','Xuan','123456',1,2),(2,'Admin',1234,'Hanoi','admin1','123456',2,1),(3,'admin2',125,'Hanoi','admin2','123456',3,2),(123,'Admin',1234,'Hanoi','admin3','123456',3,2),(23444,'admin2',125,'Hanoi','admin4','123456',2,1);
+INSERT INTO `employees` VALUES (1,'Xuan',1234,'HaNoi','Xuan','827ccb0eea8a706c4c34a16891f84e7b',1,2),(2,'Admin',1234,'Hanoi','admin1','e10adc3949ba59abbe56e057f20f883e',2,1),(3,'admin2',125,'Hanoi','admin2','e10adc3949ba59abbe56e057f20f883e',3,2),(123,'Admin',1234,'Hanoi','admin3','e10adc3949ba59abbe56e057f20f883e',3,2),(23444,'admin2',125,'Hanoi','admin4','e10adc3949ba59abbe56e057f20f883e',2,1);
 /*!40000 ALTER TABLE `employees` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `image`
+--
+
+DROP TABLE IF EXISTS `image`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `image` (
+  `request_id` int(10) NOT NULL,
+  `url_image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `image`
+--
+
+LOCK TABLES `image` WRITE;
+/*!40000 ALTER TABLE `image` DISABLE KEYS */;
+/*!40000 ALTER TABLE `image` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `information`
+--
+
+DROP TABLE IF EXISTS `information`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `information` (
+  `request_id` int(10) NOT NULL AUTO_INCREMENT,
+  `status` varchar(255) DEFAULT NULL,
+  `prioriry` varchar(255) DEFAULT NULL,
+  `ratting` varchar(255) DEFAULT NULL,
+  `reopened` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`request_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `information`
+--
+
+LOCK TABLES `information` WRITE;
+/*!40000 ALTER TABLE `information` DISABLE KEYS */;
+/*!40000 ALTER TABLE `information` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `isread`
+--
+
+DROP TABLE IF EXISTS `isread`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `isread` (
+  `request_id` int(10) NOT NULL,
+  `reader_id` int(10) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`request_id`,`reader_id`),
+  KEY `fk_ticket_read_employee` (`reader_id`),
+  CONSTRAINT `fk_ticket_read_employee` FOREIGN KEY (`reader_id`) REFERENCES `employees` (`employee_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `isread`
+--
+
+LOCK TABLES `isread` WRITE;
+/*!40000 ALTER TABLE `isread` DISABLE KEYS */;
+/*!40000 ALTER TABLE `isread` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -57,9 +164,10 @@ DROP TABLE IF EXISTS `itteam`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `itteam` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `itteam_id` int(10) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  `leader_id` int(11) NOT NULL,
+  PRIMARY KEY (`itteam_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -69,7 +177,7 @@ CREATE TABLE `itteam` (
 
 LOCK TABLES `itteam` WRITE;
 /*!40000 ALTER TABLE `itteam` DISABLE KEYS */;
-INSERT INTO `itteam` VALUES (1,'IT Hà Nội'),(2,'IT Đà Nẵng');
+INSERT INTO `itteam` VALUES (1,'IT Hà Nội',1),(2,'IT Đà Nẵng',2);
 /*!40000 ALTER TABLE `itteam` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -81,9 +189,9 @@ DROP TABLE IF EXISTS `priorities`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `priorities` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `priority_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(15) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`priority_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -105,7 +213,7 @@ DROP TABLE IF EXISTS `request`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `request` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `request_id` int(10) NOT NULL AUTO_INCREMENT,
   `subject` varchar(255) NOT NULL,
   `content` text,
   `created_by` int(10) NOT NULL,
@@ -120,13 +228,13 @@ CREATE TABLE `request` (
   `create_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   `deleted_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`request_id`),
   KEY `fk_tickets_employee` (`created_by`),
   KEY `fk_ticket_employees` (`assigned_to`),
   KEY `fk_tickets_team` (`team_id`),
-  CONSTRAINT `fk_ticket_employees` FOREIGN KEY (`assigned_to`) REFERENCES `employees` (`id`) ON UPDATE CASCADE,
-  CONSTRAINT `fk_tickets_employee` FOREIGN KEY (`created_by`) REFERENCES `employees` (`id`) ON UPDATE CASCADE,
-  CONSTRAINT `fk_tickets_team` FOREIGN KEY (`team_id`) REFERENCES `itteam` (`id`) ON UPDATE CASCADE
+  CONSTRAINT `fk_ticket_employees` FOREIGN KEY (`assigned_to`) REFERENCES `employees` (`employee_id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_tickets_employee` FOREIGN KEY (`created_by`) REFERENCES `employees` (`employee_id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_tickets_team` FOREIGN KEY (`team_id`) REFERENCES `itteam` (`itteam_id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -165,110 +273,28 @@ INSERT INTO `role` VALUES (1,'Member','all member'),(2,'Sub-Lead','medium, can m
 UNLOCK TABLES;
 
 --
--- Table structure for table `ticket_attributes`
+-- Table structure for table `subteam`
 --
 
-DROP TABLE IF EXISTS `ticket_attributes`;
+DROP TABLE IF EXISTS `subteam`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `ticket_attributes` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `status` varchar(255) DEFAULT NULL,
-  `prioriry` varchar(255) DEFAULT NULL,
-  `ratting` varchar(255) DEFAULT NULL,
-  `reopened` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `subteam` (
+  `subteam_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL,
+  `subleader_id` int(11) NOT NULL,
+  `itteam_id` int(11) NOT NULL,
+  PRIMARY KEY (`subteam_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `ticket_attributes`
+-- Dumping data for table `subteam`
 --
 
-LOCK TABLES `ticket_attributes` WRITE;
-/*!40000 ALTER TABLE `ticket_attributes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ticket_attributes` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `ticket_images`
---
-
-DROP TABLE IF EXISTS `ticket_images`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `ticket_images` (
-  `id_ticket` int(10) NOT NULL,
-  `url_image` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `ticket_images`
---
-
-LOCK TABLES `ticket_images` WRITE;
-/*!40000 ALTER TABLE `ticket_images` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ticket_images` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `ticket_reads`
---
-
-DROP TABLE IF EXISTS `ticket_reads`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `ticket_reads` (
-  `ticket_id` int(10) NOT NULL,
-  `reader_id` int(10) NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`ticket_id`,`reader_id`),
-  KEY `fk_ticket_read_employee` (`reader_id`),
-  CONSTRAINT `fk_ticket_read_employee` FOREIGN KEY (`reader_id`) REFERENCES `employees` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `ticket_reads`
---
-
-LOCK TABLES `ticket_reads` WRITE;
-/*!40000 ALTER TABLE `ticket_reads` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ticket_reads` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `ticket_threads`
---
-
-DROP TABLE IF EXISTS `ticket_threads`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `ticket_threads` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `ticket_id` int(10) NOT NULL,
-  `employee_id` int(10) NOT NULL,
-  `content` text,
-  `type` tinyint(1) DEFAULT NULL,
-  `note` varchar(255) DEFAULT NULL,
-  `create_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_tickets_ticket_thread` (`ticket_id`),
-  KEY `fk_ticket_thread_employee` (`employee_id`),
-  CONSTRAINT `fk_ticket_thread_employee` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`) ON UPDATE CASCADE,
-  CONSTRAINT `fk_tickets_ticket_thread` FOREIGN KEY (`ticket_id`) REFERENCES `request` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `ticket_threads`
---
-
-LOCK TABLES `ticket_threads` WRITE;
-/*!40000 ALTER TABLE `ticket_threads` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ticket_threads` ENABLE KEYS */;
+LOCK TABLES `subteam` WRITE;
+/*!40000 ALTER TABLE `subteam` DISABLE KEYS */;
+/*!40000 ALTER TABLE `subteam` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -280,4 +306,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-12-28 16:47:06
+-- Dump completed on 2017-12-29  9:50:45
