bcd8552fe3bb99a89dd66b340fcfd5db4e892191
doctorwhoq <doctorwhoq1@gmail.com>
2017-12-29 14:14:29 +0700

    More database-get functions update

--- src/database/RequestDb.java
+++ src/database/RequestDb.java
@@ -41,7 +41,7 @@ public class RequestDb {
                 r.setRatting(rs.getInt("rating"));
                 r.setStatus(rs.getInt("status"));
                 r.setSubject(rs.getString("subject"));
-                r.setTeamId(rs.getInt("teamID"));
+                r.setTeamId(rs.getInt("itteam_id"));
                 r.setUpdatedAt(rs.getDate("update_at"));
                 r.setResolvedAt(rs.getDate("resolved_at"));
                 r.setDeletedAt(rs.getDate("deleted_at"));
@@ -56,7 +56,7 @@ public class RequestDb {
             e.printStackTrace();
         }
 
-        // TODO
+        // TODO Exception control
         return requests;
 
     }
@@ -84,7 +84,47 @@ public class RequestDb {
                 r.setRatting(rs.getInt("rating"));
                 r.setStatus(rs.getInt("status"));
                 r.setSubject(rs.getString("subject"));
-                r.setTeamId(rs.getInt("teamID"));
+                r.setTeamId(rs.getInt("itteam_id"));
+                r.setUpdatedAt(rs.getDate("update_at"));
+                r.setResolvedAt(rs.getDate("resolved_at"));
+                r.setDeletedAt(rs.getDate("deleted_at"));
+                requests.add(r);
+            }
+
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return requests;
+    }
+
+    // get all requests assign to an employee by assigned_to and status
+    // viec toi duoc giao
+    public ArrayList<Request> getAllAssignRequest(int employeId, int status){
+        ArrayList<Request> requests = new ArrayList<Request>();
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String s = "select * from request where assigned_to = ? and status = ? ";
+            PreparedStatement statement = conn.prepareStatement(s);
+            statement.setInt(1,employeId);
+            statement.setInt(2,status);
+            ResultSet rs = statement.executeQuery();
+            while(rs.next()){
+                Request r = new Request();
+                r.setId(rs.getInt("request_id"));
+                r.setAssignedTo(rs.getInt("assigned_to"));
+                r.setClosedAt(rs.getDate("closed_at"));
+                r.setCreatedBy(rs.getInt("created_by"));
+                r.setContent(rs.getString("content"));
+                r.setDeadline(rs.getDate("deadline"));
+                r.setPriority(rs.getInt("priority"));
+                r.setRatting(rs.getInt("rating"));
+                r.setStatus(rs.getInt("status"));
+                r.setSubject(rs.getString("subject"));
+                r.setTeamId(rs.getInt("itteam_id"));
                 r.setUpdatedAt(rs.getDate("update_at"));
                 r.setResolvedAt(rs.getDate("resolved_at"));
                 r.setDeletedAt(rs.getDate("deleted_at"));
@@ -99,34 +139,97 @@ public class RequestDb {
         }
         // TODO: get from db
         return requests;
-    }
 
-    // get all requests assign to an employee by assigned_to and status
-    // viec toi duoc giao
-    public ArrayList<Request> getAllAssignRequest(int employeId, int status){
-        // TODO: get from db
-        return null;
+
     }
 
     // get all feedback of all requests assign to an employee by assigned_to and status
     // viec toi duoc giao
     public ArrayList<Request> getFeedBack(int employeId, int status){
         // TODO: get from db
+        // :??? really ?
         return null;
     }
 
     // get all request by itteam_id and status
     // cong viec cua bo phan it
     public ArrayList<Request> getAllTeamRequest(int teamId, int status){
-        // TODO: get from db
-        return null;
+        ArrayList<Request> requests = new ArrayList<Request>();
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String s = "select * from request where itteam_id = ? and status = ? ";
+            PreparedStatement statement = conn.prepareStatement(s);
+            statement.setInt(1,teamId);
+            statement.setInt(2,status);
+            ResultSet rs = statement.executeQuery();
+            while(rs.next()){
+                Request r = new Request();
+                r.setId(rs.getInt("request_id"));
+                r.setAssignedTo(rs.getInt("assigned_to"));
+                r.setClosedAt(rs.getDate("closed_at"));
+                r.setCreatedBy(rs.getInt("created_by"));
+                r.setContent(rs.getString("content"));
+                r.setDeadline(rs.getDate("deadline"));
+                r.setPriority(rs.getInt("priority"));
+                r.setRatting(rs.getInt("rating"));
+                r.setStatus(rs.getInt("status"));
+                r.setSubject(rs.getString("subject"));
+                r.setTeamId(rs.getInt("itteam_id"));
+                r.setUpdatedAt(rs.getDate("update_at"));
+                r.setResolvedAt(rs.getDate("resolved_at"));
+                r.setDeletedAt(rs.getDate("deleted_at"));
+                requests.add(r);
+            }
+
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return requests;
+
+
     }
 
     // get all request by subteam_id and status
     // cong viec cua team
-    public ArrayList<Request> getAllSubteamRequest(int subteamId, int status){
-        // TODO: get from db
-        return null;
+    public ArrayList<Request> getAllSubteamRequest(int subteamId, int status) {
+        ArrayList<Request> requests = new ArrayList<Request>();
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String s = "select * from request where subteam_id = ? and status = ? ";
+            PreparedStatement statement = conn.prepareStatement(s);
+            statement.setInt(1, subteamId);
+            statement.setInt(2, status);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                Request r = new Request();
+                r.setId(rs.getInt("request_id"));
+                r.setAssignedTo(rs.getInt("assigned_to"));
+                r.setClosedAt(rs.getDate("closed_at"));
+                r.setCreatedBy(rs.getInt("created_by"));
+                r.setContent(rs.getString("content"));
+                r.setDeadline(rs.getDate("deadline"));
+                r.setPriority(rs.getInt("priority"));
+                r.setRatting(rs.getInt("rating"));
+                r.setStatus(rs.getInt("status"));
+                r.setSubject(rs.getString("subject"));
+                r.setTeamId(rs.getInt("teamID"));
+                r.setUpdatedAt(rs.getDate("update_at"));
+                r.setResolvedAt(rs.getDate("resolved_at"));
+                r.setDeletedAt(rs.getDate("deleted_at"));
+                requests.add(r);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return requests;
     }
 
     // insert new request
